84f112a07fd547126ea4ea7b7d83203c45bdc904
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-05-05 09:44:29 -0700

    Added code example to app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-/*
-This is a test!
-*/
+const contentText = document.querySelector('.content');
+
+document.addEventListener('DOMContentLoaded', function(){
+	contentText.innerHTML = "This is the content";
+}, false);
+
+/* TEAM16 best */
